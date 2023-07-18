4dfd187b4c293b4fe24e73b5943b7d319c49189e
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-07-18 10:41:03 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return self.task
