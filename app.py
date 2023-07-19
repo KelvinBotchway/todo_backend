b1988534520aa9893b9119fa51e24203281cec24
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-07-19 23:25:11 +0000

    commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
+print("Hello world")
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 
