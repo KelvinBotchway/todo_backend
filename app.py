ea15fb27700cf3a916fd0375e6726eb58bc10932
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-07-21 21:44:56 +0000

    Commit message

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-print("Hello world")
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'  # Use SQLite for simplicity
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return self.task
+    task
