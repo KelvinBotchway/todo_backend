ee11c11dd3c3de6615805e1bf8a9ad56095b6d92
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-07-22 18:21:10 +0000

    Database add

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 db = SQLAlchemy(app)
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Boolean, default=False)
-
-    def __repr__(self):
-        return self.task
+app.app_context().push()
