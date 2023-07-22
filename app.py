d1c3a695421abe81571f36e0c15511e2363755b8
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-07-22 18:35:50 +0000

    Before adding opeartions

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 app.app_context().push()
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return self.task
+
